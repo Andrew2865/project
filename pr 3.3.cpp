fb64144420c6c2556aebb510877e2e9c713f368e
Andrew <90443266+Andrew2865@users.noreply.github.com>
2021-10-06 22:09:10 +0300

    Add files via upload

--- pr 3.3.cpp
+++ pr 3.3.cpp
@@ -1,20 +1,39 @@
-﻿// pr 3.3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿// Lab_03_3.cpp
+// < прізвище, ім’я автора >
+// Лабораторна робота No 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 0.1
 
 #include <iostream>
+#include <cmath>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	double x; // вхідний аргумент
+
+	double y; // результат обчислення виразу
+	cout << "x = "; cin >> x;
+	// розгалуження в повній формі
+	if (x < -7)
+		y = 0;
+	else
+		if (-7 < x && x < -3)
+			y = x + 7;
+		else
+			if (-3 < x && x < -2)
+				y = 4;
+			else
+				if (-2 < x && x < 2)
+					y = x * x;
+				else
+					if (2 < x && x < 4)
+						y = (-2) * x + 8;
+					else
+						y = 0;
+
+	cout << endl;
+	cout << "y = " << y << endl;
+	cin.get();
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
